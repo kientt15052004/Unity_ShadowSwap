f2efd49ca994e75256f898aa1d13f9829e52f7cd
quynzz <chinieko0@gmail.com>
2025-10-04 15:50:07 +0700

    Map

---
+++ Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        if (horizontalInput > 0.01f)
+        {
+
+            transform.localScale = new Vector3(2, 2 , 1);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-2, 2, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded == true)
+            {
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+                grounded = false;
+            }
+        }
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+}
