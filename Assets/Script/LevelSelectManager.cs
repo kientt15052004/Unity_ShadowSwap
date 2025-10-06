563f2e71d59e271d5b8c7e9f596ce9d93a2e5d4e
bactiger1508 <bacb436@gmail.com>
2025-10-06 21:21:09 +0700

    Level Screen

---
+++ Assets/Script/LevelSelectManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        // Tải màn chơi dựa trên tên Scene được truyền vào
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void GoBackToStartScreen()
+    {
+        // Quay lại màn hình chính (StartScreen)
+        // Đảm bảo tên Scene "StartScreen" trùng khớp với tên Scene gốc của bạn
+        SceneManager.LoadScene("StartScreen");
+    }
+}
