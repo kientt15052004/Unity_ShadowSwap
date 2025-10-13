c3f92dcfa046d7e0d5eda2288bf26cd4e123aead
bactiger1508 <bacb436@gmail.com>
2025-10-13 22:35:38 +0700

    Shadow Gameplay

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -9,19 +9,26 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+
+    // Thêm một biến để tham chiếu đến ShadowManager
+    private ShadowManager shadowManager;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        // Tìm và lấy đối tượng ShadowManager trong scene
+        shadowManager = FindObjectOfType<ShadowManager>();
     }
+
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
         if (horizontalInput > 0.01f)
         {
-
-            transform.localScale = new Vector3(2, 2 , 1);
+            transform.localScale = new Vector3(2, 2, 1);
         }
         else if (horizontalInput < -0.01f)
         {
@@ -36,9 +43,25 @@ public class PlayerMove : MonoBehaviour
                 grounded = false;
             }
         }
+
+        // --- Thêm logic cho Shadow Swap ---
+        // Nhấn E để tạo bản thể
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            shadowManager.CreateShadow(transform.position, transform.rotation);
+        }
+
+        // Nhấn Q để dịch chuyển đến bản thể
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            shadowManager.TeleportToShadow(transform);
+        }
+        // --- Kết thúc logic cho Shadow Swap ---
+
         anim.SetBool("Run", horizontalInput != 0);
         anim.SetBool("Grounded", grounded);
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -46,5 +69,4 @@ public class PlayerMove : MonoBehaviour
             grounded = true;
         }
     }
-
 }
