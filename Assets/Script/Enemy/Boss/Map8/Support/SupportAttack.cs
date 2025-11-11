3f05c85c5c61c56d24c287a51a1dbc183324493b
truongchibang <bangchitruong@gmail.com>
2025-11-11 09:49:53 +0700

    Update boss map 8

--- Assets/Script/Enemy/Boss/Map8/Support/SupportAttack.cs
+++ Assets/Script/Enemy/Boss/Map8/Support/SupportAttack.cs
@@ -3,37 +3,37 @@ using UnityEngine;
 
 public class SupportAttack : MonoBehaviour
 {
-    public Transform player;
-    public int damage = 10;
-    public float shootInterval = 1.5f;
+    //public Transform player;
+    //public int damage = 10;
+    //public float shootInterval = 1.5f;
 
-    private void Start()
-    {
-        if (player == null)
-        {
-            GameObject p = GameObject.FindWithTag("Player");
-            if (p != null) player = p.transform;
-        }
-        StartCoroutine(ShootLoop());
-    }
+    //private void Start()
+    //{
+    //    if (player == null)
+    //    {
+    //        GameObject p = GameObject.FindWithTag("Player");
+    //        if (p != null) player = p.transform;
+    //    }
+    //    StartCoroutine(ShootLoop());
+    //}
 
-    private IEnumerator ShootLoop()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(shootInterval);
-            TryShootAtPlayer();
-        }
-    }
+    //private IEnumerator ShootLoop()
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(shootInterval);
+    //        TryShootAtPlayer();
+    //    }
+    //}
 
-    private void TryShootAtPlayer()
-    {
-        if (player == null) return;
+    //private void TryShootAtPlayer()
+    //{
+    //    if (player == null) return;
 
-        IDamageable dmg = player.GetComponent<IDamageable>() ?? player.GetComponentInChildren<IDamageable>();
-        if (dmg != null)
-        {
-            dmg.TakeDamage(new DamageInfo(damage, transform.position, gameObject, false));
-        }
-    }
+    //    IDamageable dmg = player.GetComponent<IDamageable>() ?? player.GetComponentInChildren<IDamageable>();
+    //    if (dmg != null)
+    //    {
+    //        dmg.TakeDamage(new DamageInfo(damage, transform.position, gameObject, false));
+    //    }
+    //}
 }
--- Assets/Script/Enemy/Boss/Map8/Support/SupportHealth.cs
+++ Assets/Script/Enemy/Boss/Map8/Support/SupportHealth.cs
@@ -3,77 +3,77 @@ using UnityEngine;
 
 public class SupportHealth : MonoBehaviour
 {
-    [Header("References")]
-    public MartinalHero ownerBoss;
+    //[Header("References")]
+    //public MartinalHero ownerBoss;
 
-    [Header("Buff parameters")]
-    public float buffDamagePct = 0.25f;
-    public int buffExtraHP = 50;
-    public float buffDuration = 10f;
+    //[Header("Buff parameters")]
+    //public float buffDamagePct = 0.25f;
+    //public int buffExtraHP = 50;
+    //public float buffDuration = 10f;
 
-    [Header("Timing")]
-    public float buffInterval = 12f;
-    public float firstBuffDelay = 2f;
+    //[Header("Timing")]
+    //public float buffInterval = 12f;
+    //public float firstBuffDelay = 2f;
 
-    protected float lastBuffAt = -999f;
+    //protected float lastBuffAt = -999f;
 
-    private void Start()
-    {
-        if (ownerBoss == null)
-            ownerBoss = GetComponentInParent<MartinalHero>();
+    //private void Start()
+    //{
+    //    if (ownerBoss == null)
+    //        ownerBoss = GetComponentInParent<MartinalHero>();
 
-        if (ownerBoss != null)
-        {
-            // subscribe để tự hủy khi boss chết
-            ownerBoss.OnBossDied += OnOwnerDied;
-        }
+    //    if (ownerBoss != null)
+    //    {
+    //        // subscribe để tự hủy khi boss chết
+    //        ownerBoss.OnBossDied += OnOwnerDied;
+    //    }
 
-        StartCoroutine(BuffLoop());
-    }
+    //    StartCoroutine(BuffLoop());
+    //}
 
-    private void OnDestroy()
-    {
-        if (ownerBoss != null)
-            ownerBoss.OnBossDied -= OnOwnerDied;
-    }
+    //private void OnDestroy()
+    //{
+    //    if (ownerBoss != null)
+    //        ownerBoss.OnBossDied -= OnOwnerDied;
+    //}
 
-    private void OnOwnerDied()
-    {
-        Destroy(gameObject);
-    }
+    //private void OnOwnerDied()
+    //{
+    //    Destroy(gameObject);
+    //}
 
-    public void SetOwner(MartinalHero boss)
-    {
-        if (ownerBoss == boss) return;
+    //public void SetOwner(MartinalHero boss)
+    //{
+    //    if (ownerBoss == boss) return;
 
-        if (ownerBoss != null)
-            ownerBoss.OnBossDied -= OnOwnerDied;
+    //    if (ownerBoss != null)
+    //        ownerBoss.OnBossDied -= OnOwnerDied;
 
-        ownerBoss = boss;
-        if (ownerBoss != null)
-            ownerBoss.OnBossDied += OnOwnerDied;
-    }
+    //    ownerBoss = boss;
+    //    if (ownerBoss != null)
+    //        ownerBoss.OnBossDied += OnOwnerDied;
+    //}
 
-    IEnumerator BuffLoop()
-    {
-        yield return new WaitForSeconds(firstBuffDelay);
-        while (true)
-        {
-            TryEmitBuff();
-            yield return new WaitForSeconds(buffInterval);
-        }
-    }
+    //IEnumerator BuffLoop()
+    //{
+    //    yield return new WaitForSeconds(firstBuffDelay);
+    //    while (true)
+    //    {
+    //        TryEmitBuff();
+    //        yield return new WaitForSeconds(buffInterval);
+    //    }
+    //}
 
-    public void TryEmitBuff()
-    {
-        if (ownerBoss == null) return;
-        ownerBoss.ApplyTemporaryBuff(buffDamagePct, buffExtraHP, buffDuration);
-        lastBuffAt = Time.time;
-    }
+    //public void TryEmitBuff()
+    //{
+    //    if (ownerBoss == null) return;
+    //    ownerBoss.ApplyTemporaryBuff(buffDamagePct, buffExtraHP, buffDuration);
+    //    lastBuffAt = Time.time;
+    //}
 
-    public void OnOwnerDamaged()
-    {
-        if (Time.time > lastBuffAt + Mathf.Max(3f, buffInterval * 0.4f))
-            TryEmitBuff();
-    }
+    //public void OnOwnerDamaged()
+    //{
+    //    if (Time.time > lastBuffAt + Mathf.Max(3f, buffInterval * 0.4f))
+    //        TryEmitBuff();
+    //}
 }
